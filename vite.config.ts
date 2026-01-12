97b424705de5e02d00435adce207d8e800229874
SHOURYA GUPTA <106522476+shourya789@users.noreply.github.com>
2026-01-12 20:13:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
     },
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
     resolve: {
       alias: {
